96fc88140dc87279cbfa65abad50ea3467130c04
Tom Kwok <tom@tomkwok.com>
2021-02-16 18:17:15 +0800

    videocapture 0.2021.02.16

--- videocapture.rb
+++ videocapture.rb
@@ -4,13 +4,13 @@
 class Videocapture < Formula
   desc "VideoCapture"
   homepage "https://tomkwok.com/posts/videocapture/"
-  version "2021.02.10"
+  version "0.2021.02.16"
   bottle :unneeded
   depends_on "opencv" => "4.5"
 
   if OS.mac?
-    url "https://github.com/tomkwok/homebrew-tap/releases/download/videocapture-2021.02.10/videocapture.tar.gz"
-    sha256 "85c9c872aab24647527878c74626c4eacba1918729d7c0bb2b32ad244a23929d"
+    url "https://github.com/tomkwok/homebrew-tap/releases/download/videocapture-0.2021.02.16/videocapture.tar.gz"
+    sha256 "6a6ed7ea7ef6b1b3842b3cbacfc582c8667758d3b72fc5b904f7f7ffd82ef6b6"
   end
 
   def install
